46b248ad7d9a5b2c6345e9cd3191a8eee54c7463
kristofgazso <kristof.gazso@gmail.com>
2021-03-25 22:31:20 +0100

    Added improve-eth-ux page

--- application.py
+++ application.py
@@ -5,12 +5,12 @@ application = Flask(__name__)
 
 
 @application.route('/')
-def hello():
+def index():
     return render_template("index.html")
 
-@application.route('/predict')
-def predict():
-    return "To be added very soon ;)"
+@application.route('/improve-eth-ux')
+def improve-eth-ux():
+    return render_template("improve-eth-ux.html")
 
 if __name__ == "__main__":
 	application.debug = True
